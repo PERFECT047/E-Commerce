51a2a1b2df5688aa02b8c892b3aaa1d045b8359d
perfect047 <siddhartha.das.min19@itbhu.ac.in>
2022-04-13 19:39:58 +0530

    Final Upload

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Amazon Clone with ReactJS
+# E-Commerce with ReactJS
 
 A small web app that tries to imitate the desktop web version of amazon site, you can add items to the basket, delete them, and have your user authentification feauture thanks to Firebase.
 
-## Amazon Clone Demo Link
+## E-Commerce Demo Link
 
 You can watch the site here 
 [Click Me](https://amaznclone-v1.web.app/)
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdzRl1qcxHlCMSBS-pqBfekCWJHLqKANg",
-  authDomain: "e-commerce-e7957.firebaseapp.com",
-  projectId: "e-commerce-e7957",
-  storageBucket: "e-commerce-e7957.appspot.com",
-  messagingSenderId: "388513801638",
-  appId: "1:388513801638:web:0c3a2177ad78e777fc9b67",
-  measurementId: "G-XS3EELZ4H3",
+  apiKey: "AIzaSyC1R7yeVUyN5S5fYKsA7rTTfRerQITTqv0",
+  authDomain: "e-commerce-7dcc7.firebaseapp.com",
+  projectId: "e-commerce-7dcc7",
+  storageBucket: "e-commerce-7dcc7.appspot.com",
+  messagingSenderId: "956089450837",
+  appId: "1:956089450837:web:1b4dd8e55f2d694fd9afdc"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
